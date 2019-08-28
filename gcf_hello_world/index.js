11f8fc13d4e555d56f6a47030c21f4b96eb21f01
sandal jain <sandal@technogise.com>
2019-08-28 18:00:12 +0530

    [master]: gcloud WIP

--- gcf_hello_world/index.js
+++
@@ -1,8 +0,0 @@
-/* HTTP Cloud Function.
-*
-* @param {Object} req Cloud Function request context.
-* @param {Object} res Cloud Function response context.
-*/
-exports.helloGET = (req, res) => {
-    res.send('Hello from Cloud Functions and Cloud Source Repositories');
-};
